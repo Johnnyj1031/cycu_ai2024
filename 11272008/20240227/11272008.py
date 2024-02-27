4652f8ce75e8f32365b0d69994035698bca73b5a
WANG XING JI <g11272008@cycu.edu.tw>
2024-02-27 23:50:05 +0800

    save

--- 11272008/20240227/11272008.py
+++ 11272008/20240227/11272008.py
@@ -19,3 +19,29 @@ for entry in feed.entries:
     # 印出區隔線
     print("="*50)
     
+    import requests
+import feedparser
+import csv
+
+# RSS Feed URL
+url = "https://news.pts.org.tw/xml/newsfeed.xml"
+
+# Get RSS Feed
+response = requests.get(url)
+
+# 解析 RSS Feed
+feed = feedparser.parse(response.content)
+
+# 檔案位置
+file_path = r"C:\Users\Johnny\Desktop\cycu_ai2024\11272008.csv"
+
+# 開啟檔案，並寫入含有 "巴黎" 的標題
+with open(file_path, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Title'])  # 寫入標題
+    for entry in feed.entries:
+        if '巴黎' in entry.title:
+            print(entry.title)
+            writer.writerow([entry.title])  # 寫入含有 "巴黎" 的標題
+
+
