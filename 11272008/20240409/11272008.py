08453c5e27f783362403f5f501ec93bc7ad17dbd
WANG XING JI <g11272008@cycu.edu.tw>
2024-04-11 01:39:18 +0800

    0

--- 11272008/20240409/11272008.py
+++ 11272008/20240409/11272008.py
@@ -2,7 +2,7 @@ import pandas as pd
 import folium
 
 # 讀取CSV文件，使用'cp950'編碼
-df = pd.read_csv(r'C:\\Users\\User\Desktop\\cycu_ai2024\\11272008\\20240409\\地震活動彙整.csv', encoding='cp950')
+df = pd.read_csv(r'C:\\Users\\Johnny\\Desktop\\cycu_ai2024\\11272008\\20240409\\地震活動彙整.csv', encoding='cp950')
 
 # 刪除第一欄和第一列的資料
 df = df.drop(df.columns[0], axis=1)
@@ -14,18 +14,32 @@ df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
 # 將目前第一欄的資料只取2024-04-03 07:58:00 之後的資料
 df = df[df[df.columns[0]] >= '2024-04-03 07:58:00']
 
-# 目前第二欄的資料為經度，將其轉換成浮點數
-df[df.columns[1]] = df[df.columns[1]].astype(float)
 
-# 目前第三欄的資料為緯度，將其轉換成浮點數
-df[df.columns[2]] = df[df.columns[2]].astype(float)
+# 將第一欄的名稱改為時間，第二欄的名稱改為經度，第三欄的名稱改為緯度，第四欄的名稱改為規模，第五欄的名稱改為深度，第六欄的名稱改為地點
+df.columns = ['時間', '經度', '緯度', '規模', '深度', '地點']
+
+
+# 印出資料
+print(df)
 
 # 創建地圖對象，這裡以台灣為中心
 m = folium.Map(location=[23.6978, 120.9605], zoom_start=7)
 
-# 繪製地震點
+# 繪製地震點並在點上顯示地震資訊
 for i in range(len(df)):
-    folium.CircleMarker([df.iloc[i, 2], df.iloc[i, 1]], radius=5, color='red', fill=True, fill_color='red', fill_opacity=0.7).add_to(m)
+    # 取得緯度和經度資訊
+    latitude = df.iloc[i, 2]
+    longitude = df.iloc[i, 1]
+    
+    # 取得地震資訊
+    time = df.iloc[i, 0]
+    magnitude = df.iloc[i, 3]
+    depth = df.iloc[i, 4]
+    location = df.iloc[i, 5]
+    
+    # 在地圖上標記地震點並顯示地震資訊
+    popup_info = f"Time: {time}<br>Latitude: {latitude}<br>Longitude: {longitude}<br>Magnitude: {magnitude}<br>Depth: {depth}<br>Location: {location}"
+    folium.Marker([latitude, longitude], popup=folium.Popup(popup_info, max_width=250)).add_to(m)
 
 # 保存地圖
 m.save('earthquake_map.html')
